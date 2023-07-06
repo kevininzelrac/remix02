2375c79866ff48239825c62a9804f96648950ae5
Kevin Inzelrac <kevininzelrac@me.com>
2023-07-06 18:06:27 +0200

    update footer.tsx

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,13 +1,6 @@
 export default function Footer() {
   return (
     <footer>
-      <p>Remix - Server Side Rendering</p>
-      <p>Serverless Architecture</p>
-      <p>AWS CloudFront Distribution - S3 Bucket Frontent</p>
-      <p>
-        Lambda:Edge - GraphQL - AppSync - Cognito Authentication - DynamoDb
-        Backend
-      </p>
       <p>Web & Mobile App - built by Kevin Inzelrac</p>
     </footer>
   );
