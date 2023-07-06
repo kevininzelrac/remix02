3e6dd52d626ff620ea4a5a5f7943e24d933dd3d1
Kevin Inzelrac <kevininzelrac@me.com>
2023-07-06 18:10:06 +0200

    update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -11,8 +11,8 @@ import {
 import Nav from "./components/nav/nav";
 import Footer from "./components/footer/footer";
 import getNav from "./graphQL/query/getNav.gql";
-import awsconfig from "./services/awsconfig";
 import { getSession } from "./sessions";
+import awsconfig from "./services/awsconfig.server";
 
 import { API, Amplify } from "aws-amplify";
 Amplify.configure(awsconfig);
@@ -39,22 +39,11 @@ export const loader = async ({ request }: any) => {
   });
   const posts = data.getNav;
 
-  const env = {
-    AWS_REGION: process.env.AWS_REGION,
-    AWS_APPSYNC_GRAPHQLENDPOINT: process.env.AWS_APPSYNC_GRAPHQLENDPOINT,
-    AWS_APPSYNC_KEY: process.env.AWS_APPSYNC_KEY,
-    AWS_APPSYNC_AUTHENTICATIONTYPE: "API_KEY",
-    USER_POOL_ID: process.env.USER_POOL_ID,
-    USER_POOL_WEB_CLIENT_ID: process.env.USER_POOL_WEB_CLIENT_ID,
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
-    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  };
-
-  return { env, user, posts };
+  return { user, posts };
 };
 
 export default function App() {
-  const { env, user, posts }: any = useLoaderData();
+  const { user, posts }: any = useLoaderData();
   const navigation = useNavigation();
 
   return (
@@ -71,24 +60,11 @@ export default function App() {
           <small>0.1.17</small>
         </h1>
         <Nav data={posts} user={user} />
-        {/* <main
-        style={{
-            animationName:
-              navigation.state === "idle" ? "slideDown" : "slideUp",
-          }}
-        > */}
         <Outlet />
-        {/* </main> */}
-
         <ScrollRestoration />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.env = ${JSON.stringify(env)}`,
-          }}
-        />
         <Scripts />
         <LiveReload />
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
