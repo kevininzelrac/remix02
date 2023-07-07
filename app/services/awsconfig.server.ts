1d401bb289bf61f407e725995e86486b0b589a3b
Kevin Inzelrac <kevininzelrac@me.com>
2023-07-07 11:20:48 +0200

    update awsconfig.server.ts

--- app/services/awsconfig.server.ts
+++ app/services/awsconfig.server.ts
@@ -1,5 +1,5 @@
 const awsconfig = {
-  aws_appsync_region: process.env.AWS_REGION,
+  aws_appsync_region: process.env.AWS_APPSYNC_REGION,
   aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQLENDPOINT,
   aws_appsync_apiKey: process.env.AWS_APPSYNC_KEY,
   aws_appsync_authenticationType: "API_KEY",
