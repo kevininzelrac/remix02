3695df33420ab1257f549742834c94a3328bfa85
Kevin Inzelrac <kevininzelrac@me.com>
2023-07-25 21:03:53 +0200

    update footer.tsx

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,12 @@
+import { Link } from "@remix-run/react";
+
 export default function Footer() {
   return (
     <footer>
       <p>Web & Mobile App - built by Kevin Inzelrac</p>
+      <small>
+        <Link to="policy">Politique de confidentialité</Link>
+      </small>
     </footer>
   );
 }
