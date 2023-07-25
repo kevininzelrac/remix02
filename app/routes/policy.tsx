5a184c56a6650407ffbb3e1df85b5aad26dc9c68
Kevin Inzelrac <kevininzelrac@me.com>
2023-07-25 21:02:17 +0200

    add policy.tsx

---
+++ app/routes/policy.tsx
@@ -0,0 +1,78 @@
+export default function Policy() {
+  return (
+    <main>
+      <h3>Politique de confidentialité</h3>
+      <time>Date de la dernière mise à jour : 25/07/23</time>
+      <p>
+        Nous accordons une grande importance à la confidentialité de vos données
+        personnelles. Cette Politique de confidentialité explique comment nous
+        recueillons, utilisons, divulguons et traitons vos informations lorsque
+        vous visitez notre blog. En utilisant ce site, vous acceptez les
+        pratiques décrites dans cette Politique de confidentialité.
+      </p>
+      <ol>
+        <li>
+          Collecte de l'information Nous pouvons collecter des informations
+          personnelles identifiables telles que votre nom, adresse e-mail, et
+          autres informations que vous choisissez de fournir lorsque vous vous
+          inscrivez à notre newsletter, laissez des commentaires, ou utilisez
+          notre formulaire de contact.
+        </li>
+        <li>
+          Utilisation de l'information Les informations que nous collectons
+          peuvent être utilisées pour : Vous envoyer des newsletters ou des
+          mises à jour du blog que vous avez demandées. Améliorer l'expérience
+          utilisateur en analysant l'utilisation du blog et en nous aidant à
+          améliorer son contenu et sa convivialité. Répondre à vos commentaires
+          ou demandes via notre formulaire de contact. Fournir des informations
+          sur nos produits, services ou partenaires, si vous avez choisi de
+          recevoir de telles communications.
+        </li>
+        <li>
+          Divulgation de l'information Nous ne vendons, n'échangeons, ni ne
+          transférons vos informations personnelles identifiables à des tiers,
+          sauf dans les cas suivants : Lorsque cela est nécessaire pour répondre
+          à une demande que vous avez faite. Lorsque cela est requis par la loi
+          ou pour protéger nos droits, notre propriété ou notre sécurité, ainsi
+          que ceux de nos utilisateurs ou du public. Si vous avez donné votre
+          consentement exprès pour le partage de vos informations.
+        </li>
+        <li>
+          Protection de l'information Nous prenons la sécurité de vos données
+          personnelles très au sérieux et mettons en œuvre des mesures
+          techniques et organisationnelles pour les protéger contre tout accès
+          non autorisé, altération, divulgation ou destruction.
+        </li>
+
+        <li>
+          Cookies Notre blog peut utiliser des cookies pour améliorer
+          l'expérience de l'utilisateur. Un cookie est un petit fichier placé
+          sur votre ordinateur pour collecter des informations sur les habitudes
+          de navigation. Vous pouvez configurer votre navigateur pour refuser
+          tous les cookies ou pour vous alerter lorsque des cookies sont
+          envoyés. Cependant, certaines fonctionnalités du blog peuvent ne pas
+          fonctionner correctement sans l'utilisation de cookies.
+        </li>
+        <li>
+          Liens externes Notre blog peut contenir des liens vers des sites Web
+          tiers dont nous ne sommes pas responsables de la politique de
+          confidentialité ou du contenu. Nous vous encourageons à lire les
+          déclarations de confidentialité de ces sites.
+        </li>
+
+        <li>
+          Modifications de la Politique de confidentialité Nous nous réservons
+          le droit de modifier cette Politique de confidentialité à tout moment.
+          Toute modification sera affichée sur cette page avec une indication de
+          la date de mise à jour.
+        </li>
+        <li>
+          Consentement En utilisant notre blog, vous consentez à notre Politique
+          de confidentialité. Si vous avez des questions concernant cette
+          Politique de confidentialité, veuillez nous contacter à [votre adresse
+          e-mail de contact].
+        </li>
+      </ol>
+    </main>
+  );
+}
